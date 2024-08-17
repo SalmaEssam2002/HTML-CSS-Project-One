2dea52a3be0438e2d968f806f73bb073e025d30b
SalmaEssam2002 <soso233511@gmail.com>
2024-08-17 17:20:33 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+let message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut lab";
+let lines = document.querySelectorAll(".line");
+for(let i =0 ; i<lines.length ; i++ ){
+lines[i].innerHTML = message
+}
